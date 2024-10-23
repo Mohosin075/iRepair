c95710a827fece2fc0a7ddcd5ecfe81deb03ae93
Mohosin <mohosinali075@gmail.com>
2024-10-23 20:14:41 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <div>
+      <h2 className="font-bold text-4xl text-center text-green-500">
+        This is iRepair!
+      </h2>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
